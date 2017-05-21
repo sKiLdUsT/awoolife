fd2aaa7e2c21732e9b29b913627d68b3171857e5
sKiL <skil@skildust.com>
2017-05-21 11:13:53 +0200

    ESLint check, gulpfile reformat for JS Standard compliance

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,28 @@
-'use strict';
+'use strict'
 
-const gulp = require('gulp'),
-      sass = require('gulp-sass'),
-      babel = require('gulp-babel'),
-      concat = require('gulp-concat'),
-      uglify = require('gulp-uglify');
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const babel = require('gulp-babel')
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
+const eslint = require('gulp-eslint')
 
 // Define gulp tasks
 gulp.task('js', () => {
-    return gulp.src('js/*.js')
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(concat('app.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('public/assets'));
-});
+  return gulp.src('js/*.js')
+    .pipe(eslint.failAfterError())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/assets'))
+})
 
 gulp.task('scss', () => {
-    return gulp.src('scss/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('public/assets'))
-});
+  return gulp.src('scss/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('public/assets'))
+})
 
-gulp.task('default', ['scss', 'js'], ()=>{});
-
+gulp.task('default', ['scss', 'js'], () => {})
